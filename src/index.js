a59c873672c68e238b838fd02aef88836b690135
Mariana-Sofia <Mariana-Sofia@users.noreply.github.com>
2021-03-10 20:54:40 +0000

    Cleaning code part 1

--- src/index.js
+++ src/index.js
@@ -39,61 +39,36 @@ let weekDay = days[now.getDay()];
 let dayNumber = now.getDate();
 
 //Current Date
-let todayDate = document.querySelector("p#time");
+let todayDate = document.querySelector("#time");
 todayDate.innerHTML = `${weekDay}, ${dayNumber} ${month} ${currentHour}:${currentMinutes}`;
 
-//Today&Tomorrow Date
-let today = document.querySelector("th#today");
-today.innerHTML = `<strong>Today</strong> <br> ${dayNumber} ${month}`;
 
-let tomorrow = document.querySelector("th#tomorrow");
-tomorrow.innerHTML = `<strong>Tomorrow</strong> <br>${dayNumber + 1} ${month}`;
+function displayWeather(response){
+  console.log(response.data);
+  let temperature=document.querySelector("#temp-today");
+  let city = document.querySelector("#city");
+  let description = document.querySelector("#description");
+  let humidity = document.querySelector("#humidity");
+  let wind = document.querySelector("#wind");
+  let icon = document.querySelector("#weather-icon");
 
-//City Title
-function showTemp(response) {
-  document.querySelector("#city").innerHTML = `${response.data.name} Weather`;
-  document.querySelector("#temp-today").innerHTML = `${Math.round(
-    response.data.main.temp
-  )}ºC`;
-  document.querySelector(
-    "#humidity"
-  ).innerHTML = `${response.data.main.humidity}% 💧`;
-  let tempDescription=document.querySelector("#description")
-  tempDescription.innerHTML = `${response.data.weather[0].main}`;
+  celsiusTemp=response.data.main.temp;
+
+  temperature.innerHTML=Math.round(celsiusTemp);
+  city.innerHTML=response.data.name;
+  description.innerHTML = response.data.weather[0].description;
+  humidity.innerHTML = `💧 ${response.data.main.humidity}%`;
+  wind.innerHTML = `🌬 ${Math.round(response.data.wind.speed)}`;
+  
+  icon.setAttribute("src",`http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
 }
 
-//Current Temp
 function search(city) {
   let apiKey = "b807e5d3242a76539b823cd416c767ae";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city.value}&appid=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(showTemp);
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayWeather);
+
+  apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(nextHours);
 }
-
-function submitCity(event) {
-  event.preventDefault();
-  let city = document.querySelector("#city-input");
-  search(city);
-}
-
-let searchButton = document.querySelector("#search");
-searchButton.addEventListener("click", submitCity);
-let searchForm = document.querySelector("form");
-searchForm.addEventListener("submit", submitCity);
-search("Lisbon");
-
-function searchMyCity(position) {
-  let latitude = position.coords.latitude;
-  let longitude = position.coords.longitude;
-  let apiKey = "b807e5d3242a76539b823cd416c767ae";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(showTemp);
-}
-
-function myCity(event) {
-  event.preventDefault();
-  navigator.geolocation.getCurrentPosition(searchMyCity);
-}
-
-let currentLocation = document.querySelector("#current-location");
-currentLocation.addEventListener("click", myCity);
 
