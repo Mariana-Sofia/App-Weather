cabc140fa40a06dfbee78251fa58766c3008dd28
Mariana-Sofia <Mariana-Sofia@users.noreply.github.com>
2021-03-11 15:26:07 +0000

    styling, adding functions to buttons and links

--- src/index.js
+++ src/index.js
@@ -43,7 +43,7 @@ let todayDate = document.querySelector("#time");
 todayDate.innerHTML = `${weekDay}, ${dayNumber} ${month} ${currentHour}:${currentMinutes}`;
 
 
-function displayWeather(response){
+function showTemp(response){
   console.log(response.data);
   let temperature=document.querySelector("#temp-today");
   let city = document.querySelector("#city");
@@ -56,19 +56,70 @@ function displayWeather(response){
 
   temperature.innerHTML=Math.round(celsiusTemp);
   city.innerHTML=response.data.name;
-  description.innerHTML = response.data.weather[0].description;
+  description.innerHTML =`🌞 ${response.data.weather[0].description}`;
   humidity.innerHTML = `💧 ${response.data.main.humidity}%`;
-  wind.innerHTML = `🌬 ${Math.round(response.data.wind.speed)}`;
+  wind.innerHTML = `🌬 ${Math.round(response.data.wind.speed)} Km/h`;
   
   icon.setAttribute("src",`http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
 }
-
 function search(city) {
   let apiKey = "b807e5d3242a76539b823cd416c767ae";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(displayWeather);
-
-  apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(nextHours);
+  axios.get(apiUrl).then(showTemp);
 }
+
+function searchButton(event) {
+  event.preventDefault();
+  let searchCity = document.querySelector("#city-input");
+  search(searchCity.value);
+}
+
+function searchMyCity(position) {
+  let latitude = position.coords.latitude;
+  let longitude = position.coords.longitude;
+  let apiKey = "b807e5d3242a76539b823cd416c767ae";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(showTemp);
+}
+
+function myCity(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(searchMyCity);
+}
+
+let currentLocation = document.querySelector("#current-location");
+currentLocation.addEventListener("click", myCity);
+
+function fahrTemp(event) {
+  event.preventDefault();
+  let temp = document.querySelector("#temp-today");
+
+  celsius.classList.remove("active");
+  fahrenheit.classList.add("active");
+  let fahrenheiTemp = (celsiusTemp * 9) / 5 + 32;
+  temp.innerHTML = Math.round(fahrenheiTemp);
+}
+
+function celsiusTemp(event) {
+  event.preventDefault();
+  celsius.classList.add("active");
+  fahrenheit.classList.remove("active");
+  let temp = document.querySelector("#temp-today");
+  temp.innerHTML = Math.round(celsiusTemperature);
+}
+
+let celsiusTemperature = null;
+
+let searchForm = document.querySelector("form");
+searchForm.addEventListener("submit", searchButton);
+let button = document.querySelector("#search");
+button.addEventListener("click", searchButton);
+
+let fahrenheit = document.querySelector("#fahrenheit");
+fahrenheit.addEventListener("click", fahrTemp);
+
+let celsiusLink = document.querySelector("#celsius");
+celsiusLink.addEventListener("click", celsiusTemp);
+
+search("Lisbon");
 
